283779f7403801c5d8baea91431a866983bd1014
Victor <vtipok89@gmail.com>
2023-05-12 16:16:03 +0300

    remove old auction

--- src/auctions/auctions.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common'
-import { PrismaService } from '../database/prisma.service'
-import { UserService } from '../member/user/user.service'
-import { AuctionsResolver } from './auctions.resolver'
-import { AuctionsService } from './auctions.service'
-
-@Module({ providers: [AuctionsResolver, AuctionsService, PrismaService, UserService] })
-export class AuctionsModule {}
