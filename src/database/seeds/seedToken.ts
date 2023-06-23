0f31e33b07b9b05e5db01e78e13bff7f01560f9f
Victor <vtipok89@gmail.com>
2023-06-23 18:48:10 +0300

    some fix

--- src/database/seeds/seedToken.ts
+++ src/database/seeds/seedToken.ts
@@ -4,10 +4,8 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export async function seedToken() {
-  
     // Create 30 news articles with random user IDs
     for (let i = 0; i < 10; i++) {
-        
         const title = faker.lorem.sentence()
         const description = faker.lorem.paragraph()
 
@@ -22,7 +20,7 @@ export async function seedToken() {
                 title: title,
                 description: description,
                 points: points,
-                price: price
+                price: price,
             },
         })
     }
