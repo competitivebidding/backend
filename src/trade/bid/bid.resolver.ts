969d24fa2badbb5b5e1e429abe58f71848d6e26f
MalinavyBusel <egorbgtrvch@gmail.com>
2023-07-15 23:04:42 +0300

    added users limit logic to auctions and some commented questions about business logic

--- src/trade/bid/bid.resolver.ts
+++ src/trade/bid/bid.resolver.ts
@@ -1,8 +1,8 @@
-import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { ConfigService } from '@nestjs/config'
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { GetCurrentUserId } from '../../auth/decorators'
-import { BidService } from './bid.service'
 import { AuctionService } from '../auction/auction.service'
+import { BidService } from './bid.service'
 import { BidInput } from './dto/bid.input'
 import { CreateBidInput } from './dto/create-bid.input'
 import { UpdateBidInput } from './dto/update-bid.input'
@@ -10,7 +10,11 @@ import { Bid } from './entities/bid.entity'
 
 @Resolver(() => Bid)
 export class BidResolver {
-    constructor(private readonly bidsService: BidService, private readonly config: ConfigService) {}
+    constructor(
+        private readonly bidsService: BidService,
+        private readonly auctionsService: AuctionService,
+        private readonly config: ConfigService,
+    ) {}
 
     @Query(() => [Bid])
     async getBidsByAuctionId(
@@ -42,23 +46,44 @@ export class BidResolver {
 
     @Mutation(() => Bid)
     async createMyBid(@GetCurrentUserId() userId: number, @Args('input') input: CreateBidInput) {
-        // TODO - check enough user token to do this
+        // TODO - check enough user tokens to do this
         // TODO - check if auction exists
 
+        const maxUsers = this.config.get<number>('MAX_PARTICIPANTS')
         const { bitPrice, auctionId } = input
-        const participants = await this.bidsService.countParticipantsWithoutUser(auctionId, userId)
-        if (participants >= this.config.get<number>('MAX_PARTICIPANTS')) {
-            throw new Error('cannot create a bid: max number of participants')
-        }
-
         const inputBid = {
             user: { connect: { id: userId } },
             auction: { connect: { id: auctionId } },
             bitPrice,
         }
+
+        // вообще конечно мб лучше сделать, чтобы при проверке того, что есть аукцион(это в туду есть),
+        // сразу тащить
+        // его статус и участвует ли этот юзер, и по этим данным в случае чего не пускать новый бид
+
+        // и в целом, наверное, не очень догоняю, у нас можно создавать новые биды каждому юзеру,
+        // или обновлять старые, ибо у нас и тут логика новых, и логика обновления старых методом ниже
+        const participants = await this.bidsService.countParticipantsWithoutUser(auctionId, userId)
+        if (participants >= maxUsers) {
+            throw new Error('cannot create a bid: max number of participants')
+        }
+
         const bid = await this.bidsService.createMyBid(inputBid)
         if (!bid) {
             throw new Error('Cannot create bid')
+        }
+
+        // если кап участников - аукцион закрываем
+        if (participants + 1 == maxUsers) {
+            const auctionInput = {
+                status: { connect: { id: 3 } }, // status with id=3 is "Closed"
+                // я бы добавил в схему статуса ограничение на уникальность имени и прописал здесь
+                // по имени явно, тк вдруг будем обновлять базу и айдишки статусов изменятся
+            }
+            const auct = await this.auctionsService.updateAuction(userId, auctionId, auctionInput)
+            if (!auct) {
+                throw new Error('Cannot update auction status')
+            }
         }
 
         return bid
