ba02c495b514bd75c427a7f75140d7636f7096ee
Elias <worldlolxd121@gmail.com>
2023-12-29 00:41:50 +0300

    fix: update in class RotoSub field balanse to balance

--- src/pay/dto/roto-subscription.response.ts
+++ src/pay/dto/roto-subscription.response.ts
@@ -3,12 +3,12 @@ import { Field, Int, ObjectType } from '@nestjs/graphql'
 @ObjectType()
 export class RotoSub {
     constructor(userId: number, balanse: number) {
-        ;(this.userId = userId), (this.balanse = balanse)
+        ;(this.userId = userId), (this.balance = balanse)
     }
 
     @Field(() => Int)
     userId: number
 
     @Field(() => Int)
-    balanse: number
+    balance: number
 }
